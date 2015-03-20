76613437faadca3c62ece9bb5b855005d89d748a
oops <givitru@gmail.com>
2015-03-20 19:29:40 +0200

    new branch

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/**
- * Created by oops on 13.03.15.
 var express = require('express');
 var app = express();
 var loger = require('morgan');
@@ -17,7 +15,7 @@ require ('./routers/index.js')(app);
 
 app.listen(3030, function(){
     console.log ('------- Express Sart Success -------')
-});*/
+});
 
 var express = require('express');
 var app = express();
